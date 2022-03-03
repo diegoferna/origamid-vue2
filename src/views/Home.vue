cc24b96c74d267f2a9aff4a3600c1bc0b0dce6e3
diegoferna-polo <diego.nascimento@polo-it.com>
2022-03-03 11:23:00 -0300

    insert products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>👊 Compre ou Venda 👌</h1>
+    <produtos-buscar />
+    <produtos-lista />
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import ProdutosBuscar from "../components/ProdutosBuscar.vue";
+import ProdutosLista from "../components/ProdutosLista.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: { ProdutosBuscar, ProdutosLista },
 };
 </script>
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
